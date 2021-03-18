f769c5e53687024a2bf37ece67447b1ba00243e4
Vando Silva <shdzin@hotmail.com>
2021-03-17 22:47:55 -0300

    OrderItem, many-to-many association with extra attributes PART 2

--- src/main/java/com/studyvando/course/config/TestConfig.java
+++ src/main/java/com/studyvando/course/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.studyvando.course.entities.Category;
 import com.studyvando.course.entities.Order;
+import com.studyvando.course.entities.OrderItem;
 import com.studyvando.course.entities.Product;
 import com.studyvando.course.entities.User;
 import com.studyvando.course.entities.enums.OrderStatus;
 import com.studyvando.course.repositories.CategoryRepository;
+import com.studyvando.course.repositories.OrderItemRepository;
 import com.studyvando.course.repositories.OrderRepository;
 import com.studyvando.course.repositories.ProductRepository;
 import com.studyvando.course.repositories.UserRepository;
@@ -34,6 +36,8 @@ public class TestConfig implements CommandLineRunner {
 	@Autowired
 	private ProductRepository productRepository;
 	
+	@Autowired
+	private OrderItemRepository orderItemRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -53,7 +57,7 @@ public class TestConfig implements CommandLineRunner {
 		
 		p1.getCategories().add(cat2);
 		p2.getCategories().add(cat1);
-		p2.getCategories().add(cat3);
+		p2.getCategories().add(cat3); 
 		p3.getCategories().add(cat3);
 		p4.getCategories().add(cat3);
 		p5.getCategories().add(cat2);
@@ -69,7 +73,14 @@ public class TestConfig implements CommandLineRunner {
 		
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+
+		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice()); 
+		OrderItem oi2 = new OrderItem(o1, p3, 1, p4.getPrice()); 
+		OrderItem oi3 = new OrderItem(o2, p3, 2, p1.getPrice()); 
+		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
+		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+	
 	}
 
 }
