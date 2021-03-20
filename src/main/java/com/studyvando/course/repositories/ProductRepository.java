7946c6dc9621f3f0421681fe0ebe4dc6a07829cd
Vando Silva <shdzin@hotmail.com>
2021-03-19 22:39:34 -0300

    Product-OrderItem one-to-many association

--- src/main/java/com/studyvando/course/repositories/ProductRepository.java
+++ src/main/java/com/studyvando/course/repositories/ProductRepository.java
@@ -2,7 +2,6 @@ package com.studyvando.course.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.studyvando.course.entities.Category;
 import com.studyvando.course.entities.Product;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
